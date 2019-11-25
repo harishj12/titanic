01ba9b8eacfd4fa367df088666ee17f4cf37c328
Harish Jardawala <38352823+harishj12@users.noreply.github.com>
2019-11-25 21:28:08 +0530

    Add files via upload

--- TitanicProblem.py
+++ TitanicProblem.py
@@ -23,12 +23,10 @@ uploaded = files.upload()
 from sklearn import linear_model
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.linear_model import Perceptron
 from sklearn.linear_model import SGDClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC, LinearSVC
-from sklearn.naive_bayes import GaussianNB
 
 test_m = pd.read_csv("test.csv")
 train_m = pd.read_csv("train.csv")
